7b438d5719b19636b2f196fd9e6deed307562740
babluchandran <babluchandran@users.noreply.github.com>
2017-08-10 17:19:53 +0530

    Update env.js

--- OnlineShoppingCart/src/main/resources/static/app/env.js
+++ OnlineShoppingCart/src/main/resources/static/app/env.js
@@ -10,10 +10,10 @@
 
   // API url
   //window.__env.authserviceUrl = 'http://localhost:8001';
-  window.__env.authserviceUrl = 'http://authservice.cloudapps-b488.oslab.opentlc.com/';
-  window.__env.catalogueserviceUrl = 'http://catalogueservice.cloudapps-b488.oslab.opentlc.com/';
-  window.__env.orderserviceUrl = 'http://orderservice.cloudapps-b488.oslab.opentlc.com/';
-  window.__env.shippingserviceUrl = 'http://shippingservice.cloudapps-b488.oslab.opentlc.com/';
+  window.__env.authserviceUrl = 'http://authservice:8001';
+  window.__env.catalogueserviceUrl = 'http://catalogueservice:8022';
+  window.__env.orderserviceUrl = 'http://orderservice:8088';
+  window.__env.shippingserviceUrl = 'http://shippingservice:8084';
 
   // Base url
   window.__env.baseUrl = '/';
